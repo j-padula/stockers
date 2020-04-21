a717e65b2feb7efca89c4dd67719f16b4409f188
Javier Padula <padulajavier1@gmail.com>
2020-04-21 20:31:54 +0200

    Inicio del proyecto

---
+++ src/main/java/com/stockers/stockers/StockersApplication.java
@@ -0,0 +1,13 @@
+package com.stockers.stockers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockers/stockers/StockersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockers.stockers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
